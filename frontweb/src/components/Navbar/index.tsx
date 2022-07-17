537204749295e5b0934a50bceec890a7af669168
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-17 14:54:55 -0300

    chore(frontweb): navbar styling

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid ">
+        <a href="link" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscarrostop-navbar"
+          aria-controls="dscarrostop-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscarrostop-navbar">
+          <ul className="navbar-nav ms-md-auto main-menu">
+            <li>
+              <a href="link" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
