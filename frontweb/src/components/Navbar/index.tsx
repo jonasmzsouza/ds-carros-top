cafb8fd7fd2d4db80abef70a5d36d03c1ea05553
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-19 19:58:49 -0300

    fix: navbar

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/js/src/collapse.js'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
-      <div className="container-fluid ">
+      <div className="container">
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
